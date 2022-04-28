3456295ad634cd65fad08f4c292c9defc8ee960d
garimasingh26 <102891674+garimasingh26@users.noreply.github.com>
2022-04-28 18:32:39 +0530

    Create day2.cpp

---
+++ garimasingh/docc.c++/day2.cpp
@@ -0,0 +1,12 @@
+// Write a program in C++ to print the sum of two numbers using variables.
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+	int a,b,sum;
+	a=29;
+	b=30;
+	sum=a+b;
+	cout << "The sum of two numbers "<<a<<" and "<<b<<" : "<<sum;
+}

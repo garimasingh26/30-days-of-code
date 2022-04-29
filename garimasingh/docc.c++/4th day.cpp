6188cb4dabca0488f5a14304407161bfcff58adc
garimasingh26 <102891674+garimasingh26@users.noreply.github.com>
2022-04-29 16:37:46 +0530

    Create 4th day.cpp

---
+++ garimasingh/docc.c++/4th day.cpp
@@ -0,0 +1,14 @@
+// Write a C++ program to print first 10 natural numbers using for loop.
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "The First 10 Natural Numbers are\n";
+	
+	for (int i = 1; i <= 10; i++)
+	{
+		cout << i << "\n";
+	}
+}

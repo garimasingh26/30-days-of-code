a15fd361ad4de7c572903033e2239e5cc49a5fb2
garimasingh26 <102891674+garimasingh26@users.noreply.github.com>
2022-05-06 22:06:40 +0530

    Create day9.cpp

---
+++ garimasingh/docc.c++/day9.cpp
@@ -0,0 +1,30 @@
+// Write a C++ Program to find Sum of odd numbers between 1-100 using class
+
+#include<iostream>
+using namespace std;
+class sum
+{
+   private:
+        int n,s=0;
+        public:
+          void calc();
+                void display();
+                
+};
+void sum::calc()
+{
+        for(n=1;n<=100;n+=2)
+        s=s+n;
+        
+}
+void sum::display()
+{
+        cout<<"Sum of all odd numbers between 1-100:"<<s;
+}
+int main()
+{
+        sum s;
+        s.calc();
+        s.display();
+        
+}
